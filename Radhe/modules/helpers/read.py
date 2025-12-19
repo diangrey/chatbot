dceac04cc9f94210d601e1a4d445931b333d8e17
diangrey <diangreyling0705@gmail.com>
2025-12-19 12:19:22 +0530

    Create read.py

---
+++ Radhe/modules/helpers/read.py
@@ -0,0 +1,58 @@
+from config import OWNER_USERNAME, SUPPORT_GRP
+from Radhe import Radhe
+
+START = f"""
+**๏ ʜᴇʏ, ɪ ᴀᴍ [{Radhe.name}](t.me/{Radhe.username})**
+**➻ ᴀɴ ᴀɪ ʙᴀsᴇᴅ ᴄʜᴀᴛʙᴏᴛ**
+**──────────────**
+**➻ ᴜsᴀɢᴇ /chatbot [ᴏɴ/ᴏғғ]**
+<b>||๏ ʜɪᴛ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ғᴏʀ ʜᴇʟᴘ.||</b>
+"""
+
+HELP_READ = f"""
+<u>**ᴄᴏᴍᴍᴀɴᴅs ғᴏʀ {Radhe.name}**</u>
+<u>**ᴀʀᴇ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ!**</u>
+**ᴀʟʟ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ:/**
+**──────────────**
+<b>||©️ @{OWNER_USERNAME}||</b>
+"""
+
+TOOLS_DATA_READ = f"""
+<u>**ᴛᴏᴏʟs ғᴏʀ {Radhe.name} ᴀʀᴇ:**</u>
+**➻ ᴜsᴇ /repo ғᴏʀ ɢᴇᴛᴛɪɴɢ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ!**
+**──────────────**
+**➻ ᴜsᴇ /ping ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴛʜᴇ ᴘɪɴɢ ᴏғ {Radhe.name}**
+**──────────────**
+**➻ ᴜsᴇ /id ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ, ᴄʜᴀᴛ ɪᴅ ᴀɴᴅ ᴍᴇssᴀɢᴇ ɪᴅ ᴀʟʟ ɪɴ ᴀ sɪɴɢʟᴇ ᴍᴇssᴀɢᴇ.**
+**──────────────**
+<b>||©️ @{OWNER_USERNAME}||</b>
+"""
+
+CHATBOT_READ = f"""
+<u>**ᴄᴏᴍᴍᴀɴᴅs ғᴏʀ {Radhe.name}**</u>
+**➻ ᴜsᴇ /chatbot ᴛᴏ ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ ᴛʜᴇ ᴄʜᴀᴛʙᴏᴛ.**
+**๏ ɴᴏᴛᴇ ➻ ᴛʜᴇ ᴀʙᴏᴠᴇ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀ ᴄʜᴀᴛʙᴏᴛ ᴡᴏʀᴋ ɪɴ ɢʀᴏᴜᴘ ᴏɴʟʏ!!**
+**───────────────**
+<b>||©️ @{OWNER_USERNAME}||</b>
+"""
+
+SOURCE_READ = f"""
+**ʜᴇʏ, ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴏғ [{Radhe.name}](https://t.me/{Radhe.username}) ɪs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ.**
+**ᴘʟᴇᴀsᴇ ғᴏʀᴋ ᴛʜᴇ ʀᴇᴘᴏ & ɢɪᴠᴇ ᴛʜᴇ sᴛᴀʀ ✯**
+**──────────────────**
+**ʜᴇʀᴇ ɪs ᴛʜᴇ [sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ](https://github.com/DAXXTEAM/DAXXCHATBOT)**
+**──────────────────**
+**ɪғ ʏᴏᴜ ғᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴛʜᴇɴ ᴄᴏɴᴛᴀᴄᴛ ᴀᴛ [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](https://t.me/{SUPPORT_GRP}).**
+<b>||©️ @{OWNER_USERNAME}||</b>
+"""
+
+ADMIN_READ = f"sᴏᴏɴ"
+
+ABOUT_READ = f"""
+**➻ [{Radhe.name}](https://t.me/{Radhe.username}) ɪs ᴀɴ ᴀɪ ʙᴀsᴇᴅ ᴄʜᴀᴛ-ʙᴏᴛ.**
+**➻ [{Radhe.name}](https://t.me/{Radhe.username}) ʀᴇᴘʟɪᴇs ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴛᴏ ᴀ ᴜsᴇʀ.**
+**➻ ʜᴇʟᴘs ʏᴏᴜ ɪɴ ᴀᴄᴛɪᴠᴀᴛɪɴɢ ʏᴏᴜʀ ɢʀᴏᴜᴘs.**
+**➻ ᴡʀɪᴛᴛᴇɴ ɪɴ [ᴘʏᴛʜᴏɴ](https://www.python.org) ᴡɪᴛʜ [ᴍᴏɴɢᴏ-ᴅʙ](https://www.mongodb.com) ᴀs ᴀ ᴅᴀᴛᴀʙᴀsᴇ**
+**──────────────**
+**➻ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ғᴏʀ ɢᴇᴛᴛɪɴɢ ʙᴀsɪᴄ ʜᴇʟᴩ ᴀɴᴅ ɪɴғᴏ ᴀʙᴏᴜᴛ [{Radhe.name}](https://t.me/{Radhe.username})**
+"""
